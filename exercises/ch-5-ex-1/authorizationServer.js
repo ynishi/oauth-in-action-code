d87d633484baa8c05846162d9a74d55a51b0a8a6
Yutaka Nishimura <ytk.nishimura@gmail.com>
2019-12-22 21:06:40 +0900

    5.2.2

--- exercises/ch-5-ex-1/authorizationServer.js
+++ exercises/ch-5-ex-1/authorizationServer.js
@@ -71,6 +71,38 @@ app.post("/approve", function(req, res) {
   /*
    * Process the results of the approval page, authorize the client
    */
+  var reqid = req.body.reqid;
+  var query = requests[reqid];
+  delete requests[repid];
+
+  if (!query) {
+    res.render("error", { error: "No matching authorization request" });
+    return;
+  }
+  if (req.body.approve) {
+  } else {
+    var urlParsed = buildUrl(query.redirect_uri, {
+      error: "access_denied"
+    });
+    res.redirect(urlParsed);
+    return;
+  }
+  if (query.response_type == "code") {
+    var code = randomstring.generate(8);
+    codes[code] = { request: query };
+    var urlParsed = buildUrl(query.redirect_uri, {
+      code: code,
+      state: query.state
+    });
+    res.redirect(urlParsed);
+    return;
+  } else {
+    var urlParsed = buildUrl(query.redirect_uri, {
+      error: "unsupported_response_type"
+    });
+    res.redirect(urlParsed);
+    return;
+  }
 });
 
 app.post("/token", function(req, res) {
