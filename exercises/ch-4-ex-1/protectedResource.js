df7ea9c291ced48b7d06b833e8de82d93f729444
Yutaka Nishimura <ytk.nishimura@gmail.com>
2019-12-22 14:20:01 +0900

    4.1

--- exercises/ch-4-ex-1/protectedResource.js
+++ exercises/ch-4-ex-1/protectedResource.js
@@ -1,52 +1,57 @@
 var express = require("express");
-var bodyParser = require('body-parser');
-var cons = require('consolidate');
-var nosql = require('nosql').load('database.nosql');
-var __ = require('underscore');
-var cors = require('cors');
+var bodyParser = require("body-parser");
+var cons = require("consolidate");
+var nosql = require("nosql").load("database.nosql");
+var __ = require("underscore");
+var cors = require("cors");
 
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true })); // support form-encoded bodies (for bearer tokens)
 
-app.engine('html', cons.underscore);
-app.set('view engine', 'html');
-app.set('views', 'files/protectedResource');
-app.set('json spaces', 4);
+app.engine("html", cons.underscore);
+app.set("view engine", "html");
+app.set("views", "files/protectedResource");
+app.set("json spaces", 4);
 
-app.use('/', express.static('files/protectedResource'));
+app.use("/", express.static("files/protectedResource"));
 app.use(cors());
 
 var resource = {
-	"name": "Protected Resource",
-	"description": "This data has been protected by OAuth 2.0"
+  name: "Protected Resource",
+  description: "This data has been protected by OAuth 2.0"
 };
 
 var getAccessToken = function(req, res, next) {
-	/*
-	 * Scan for an access token on the incoming request.
-	 */
-	
+  /*
+   * Scan for an access token on the incoming request.
+   */
+  var inToken = null;
+  var auth = req.headers["authorization"];
+  if (auth && auth.toLowerCase().indexOf("bearer") == 0) {
+    inToken = auth.slice("bearer ".length);
+  } else if (req.body && req.body.access_token) {
+    inToken = req.body.access_token;
+  } else if (req.query && req.query.access_token) {
+    inToken = req.query.access_token;
+  }
+  console.log(inToken);
 };
 
-app.options('/resource', cors());
-
+app.options("/resource", cors());
 
 /*
  * Add the getAccessToken function to this handler
  */
-app.post("/resource", cors(), function(req, res){
-
-	/*
-	 * Check to see if the access token was found or not
-	 */
-	
+app.post("/resource", cors(), function(req, res) {
+  /*
+   * Check to see if the access token was found or not
+   */
 });
 
-var server = app.listen(9002, 'localhost', function () {
+var server = app.listen(9002, "localhost", function() {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('OAuth Resource Server is listening at http://%s:%s', host, port);
+  console.log("OAuth Resource Server is listening at http://%s:%s", host, port);
 });
- 
